52f489ac459733fe8c5419944efe16c00d91945b
David Loewe <49597367+TumbleOwlee@users.noreply.github.com>
2023-11-10 13:19:43 +0100

    Update connect.py

--- Windows/Scripts/connect.py
+++ Windows/Scripts/connect.py
@@ -13,13 +13,37 @@ from inquirer import themes
 # Terminal for colorization
 from blessed import Terminal
 # compile for input parsing
-from parse import compile
+from parse import parse
 
-# List of all supported hosts, either single value "<value>" or aliased "<alias> [<value>]"
+# Customize theme of inquirer
+term = Terminal()
+theme = themes.Default()
+theme.List.selection_color = term.yellow
+
+# Host class holding all info
+class Host:
+    def __init__(self, url = None, user = None, port = None, alias = None):
+        self.url = url
+        self.user = user
+        self.port = port
+        self.alias = alias
+
+    def _colorize(self):
+        user = f"{term.blue}{self.user}{term.yellow}@{term.normal}" if self.user else ""
+        url = f"{term.yellow}{self.url}{term.normal}" if self.url else ""
+        port = f"{term.green}:{self.port}{term.normal}" if self.port else ""
+        if self.url:
+            return f"{self.alias} {term.normal}[{user}{url}{port}{term.normal}]"
+        else:
+            return self.alias
+
+    def __str__(self):
+        return self._colorize()
+
+# List of all configured hosts
 host_list = [
-    'ubuntu-server [127.0.0.1]',
-    'arch-server [127.0.0.1]',
-    'some-domain',
+    Host("url", "user", "port", "alias"),
+    Host(None, None, None, "Other..")
 ]
 
 # Print info
@@ -33,12 +57,12 @@ def signal_handler(sig, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 # Ask for text input with default value handling
-def ask_text(prompt: str, default: str = None, theme=themes.Default(), extract=None):
+def ask_text(prompt: str, default: str = None, theme=themes.Default()):
     message = f"{prompt} [{default}]" if default else f"{prompt}"
     questions = [inquirer.Text('input', message=message)]
     answer = inquirer.prompt(questions, theme=theme)
     if answer and answer['input']:
-        value = extract(answer['input']) if extract else answer['input']
+        value = answer['input']
         info(f"Received value '{value}'.")
         return value
     elif answer:
@@ -48,11 +72,11 @@ def ask_text(prompt: str, default: str = None, theme=themes.Default(), extract=N
         sys.exit(1)
 
 # Ask for list choice
-def ask_list(prompt: str, choices: [str], theme=themes.Default(), extract=None):
+def ask_list(prompt: str, choices: [str], theme=themes.Default()):
     questions = [inquirer.List('input', message=prompt, choices=choices)]
     answer = inquirer.prompt(questions, theme=theme)
     if answer and answer['input']:
-        value = extract(answer['input']) if extract else answer['input']
+        value = answer['input']
         info(f"Received value '{value}'.")
         return value
     elif answer:
@@ -66,26 +90,17 @@ def execute(cmd=[str]):
     print(f"[{term.orangered}!{term.normal}] Execute '{cmd_line}'")
     subprocess.run(cmd)
 
-# Customize theme of inquirer
-term = Terminal()
-theme = themes.Default()
-theme.List.selection_color = term.goldenrod1
-
-# Extract host IP if aliased
-def extract_value_if_aliased(input_str: str):
-    parser = compile("{} [{}]")
-    parsed = parser.parse(input_str)
-    return parsed[1] if parsed else input_str
-
 # Get user input
-user = ask_text("What's your username?", default="dloewe", theme=theme)
-port = ask_text("What port to use?", default=22, theme=theme)
-host = ask_list("What host to connect to?", choices=host_list + ['Not listed'], theme=theme, extract=extract_value_if_aliased)
-if not host or host == "Not listed":
-    host = ask_text("Specify name of host", theme=theme)
+host = ask_list("What host to connect to?", choices=host_list, theme=theme)
+if host.url == None:
+    host.url = ask_text("Specify name of host", theme=theme)
+if host.user == None:
+    host.user = ask_text("What's your username?", default=host.user or os.environ.get('USER', os.environ.get('USERNAME')), theme=theme)
+if host.port == None:
+    host.port = ask_text("What port to use?", default=host.port or 22, theme=theme)
 
 # Run SSH
-execute(['ssh', '-p', f"{port}", f"{user}@{host}"])
+execute(['ssh', '-p', f"{host.port}", f"{host.user}@{host.url}"])
 
 # Terminating
 info("Terminating.")
