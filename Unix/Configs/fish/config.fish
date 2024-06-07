e7bd0d7c80e8e7527ddf0817df60c28ce9492330
David Loewe <49597367+TumbleOwlee@users.noreply.github.com>
2024-06-07 17:16:51 +0200

    Create config.fish

---
+++ Unix/Configs/fish/config.fish
@@ -0,0 +1,22 @@
+function __colored_cat
+    set -l argc (count $argv)
+    if test $argc -gt 0
+        echo "" >&2
+        for var in (seq 1 $argc)
+            echo "  ================================================================================================" >&2
+            echo "    Filename : $argv[$var]" >&2
+            echo "  ================================================================================================" >&2
+            echo "" >&2
+            set -l lines (pygmentize -g -O style=gruvbox-dark $argv[$var] | /usr/bin/cat -n)
+            for line in $lines
+                set -l lineno (echo "$line" | cut -f1)
+                set -l lineco (echo "$line" | cut -f2- | sed 's/\t/    /g')
+                echo -n "$lineno  " >&2
+                echo $lineco
+            end
+            echo "" >&2
+        end
+    end
+end
+
+alias ccat=__colored_cat
