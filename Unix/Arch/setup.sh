f493108e36f11ef32bc00f648825d8adfc900d12
David Loewe <49597367+TumbleOwlee@users.noreply.github.com>
2025-05-03 20:15:29 +0200

    Update setup.sh

--- Unix/Arch/setup.sh
+++ Unix/Arch/setup.sh
@@ -19,7 +19,11 @@ check_proxy
 resp=$(ask "Update and upgrade? [Y/n]" "Y")
 if [ "_$resp" != "_n" ] && [ "_$resp" != "_N" ]; then
     info "Update and upgrade."
-    STDOUT="cout" STDERR="cerr" run_with_retry pacman -Syyu --noconfirm
+    if [ -z "$SUDO" ]; then
+        STDOUT="cout" STDERR="cerr" run_with_retry pacman -Syyu --noconfirm
+    else
+        STDOUT="cout" STDERR="cerr" run_with_retry $SUDO pacman -Syyu --noconfirm
+    fi
 fi
 
 # Install yay
