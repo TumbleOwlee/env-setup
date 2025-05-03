45971ad6280ff777d881d5a97328a8ea78def5be
David Loewe <49597367+TumbleOwlee@users.noreply.github.com>
2025-05-03 19:57:20 +0200

    Update setup.sh

--- Unix/Arch/setup.sh
+++ Unix/Arch/setup.sh
@@ -27,15 +27,17 @@ run_with_retry $SUDO pacman -S --needed --noconfirm git base-devel less
 if [ ! -z "$(whoami)" ] && [ "$(whoami)" == "root" ]; then
     run_with_retry sudo -u nobody git clone https://aur.archlinux.org/yay-bin.git /tmp/yay-bin
     cd /tmp/yay-bin
-    STDOUT="cout" STDERR="cerr" run_with_retry sudo -u nobody makepkg -s
+    STDOUT=/dev/null STDERR=/dev/null run_with_retry sudo -u nobody makepkg -s
     STDOUT=/dev/null STDERR=/dev/null run_once rm /tmp/yay-bin/yay-bin-debug*.pkg.tar.zst
-    run_with_retry pacman -U --noconfirm /tmp/yay-bin/yay-bin-*.pkg.tar.zst
+    STDOUT=/dev/null STDERR=/dev/null un_with_retry pacman -U --noconfirm /tmp/yay-bin/yay-bin-*.pkg.tar.zst
     cd -
     rm -rf /tmp/yay-bin
 else
     run_with_retry git clone https://aur.archlinux.org/yay-bin.git /tmp/yay-bin
     cd /tmp/yay-bin
-    STDOUT="cout" STDERR="cerr" run_with_retry makepkg -si
+    STDOUT=/dev/null STDERR=/dev/null run_with_retry makepkg -s
+    STDOUT=/dev/null STDERR=/dev/null run_once rm /tmp/yay-bin/yay-bin-debug*.pkg.tar.zst
+    STDOUT="cout" STDERR="cerr" un_with_retry $SUDO pacman -U --noconfirm /tmp/yay-bin/yay-bin-*.pkg.tar.zst
     cd -
     rm -rf /tmp/yay-bin
 fi
